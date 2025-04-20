bf87ecb5911227760094c75ce2f5e902edd828e4
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-04-19 19:49:31 -0700

    Module 2 - Exercise 2: added changes to the Cell Phone Service App

--- CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhone.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 //STEP 1:
     public class CellPhone {
 
-    //Variables:
+    //Variables for user input:
     private int serialNumber;
     private String model;
     private String carrier;
@@ -17,6 +17,16 @@ package com.pluralsight;
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
+
+    }
+
+    public void StrangerPhone(){
+        this.serialNumber = 101010;
+        this.model = "iPhone 15";
+        this.carrier = "Verizon";
+        this.phoneNumber = "(123)456-7890";
+        this.owner = "Stranger";
+
     }
 
     //Getters: special methods that always begin with get - they allow us to read the value of a variable from a class
@@ -61,11 +71,17 @@ package com.pluralsight;
         this.owner = owner;
     }
 
-    //for this string to work, make sure you use return to get your string to print out
+    //Exercise 2 - Module 2 - STEP 1: Add a dial method
+    public void dial(String phoneToDial){
+        System.out.println(owner + "'s phone is calling " + phoneToDial);
+    }
+
+    //for a public String to work, make sure you use return to get your string to print out
     public String CellPhoneInfo(){
         System.out.println();
         System.out.println("-------Cell Phone Info-------");
         return "Serial number: " + getSerialNumber() + "\nModel: " + getModel() + "\nCarrier: " + getCarrier() + "\nPhone number: " + getPhoneNumber() + "\nOwner: " + getOwner();
+
     }
 }
 
--- CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
+++ CellPhoneService/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -7,7 +7,13 @@ public class CellPhoneApplication{
 
     public static void main(String[] args) {
 
+
+        //Exercise 1: 1st instance of a cell:
         CellPhone cell1 = new CellPhone();
+        //Exercise 2: 2nd instance of a cell:
+        CellPhone strangerPhone = new CellPhone();
+        //Calling the StrangerPhone method from CellPhone class:
+        strangerPhone.StrangerPhone();
 
         System.out.println("Enter the cell serial number: ");
         int serialNumber = read.nextInt();
@@ -30,8 +36,23 @@ public class CellPhoneApplication{
         String owner = read.nextLine();
         cell1.setOwner(owner);
 
-        System.out.println(cell1.CellPhoneInfo());
+        display(cell1);
+        display(strangerPhone);
 
-        read.close();
+        System.out.println("-----------------------------\n ");
+
+        System.out.println();
+
+        cell1.dial(strangerPhone.getPhoneNumber());
+        strangerPhone.dial(cell1.getPhoneNumber());
+
     }
+
+
+    public static void display(CellPhone phone){
+        //(CellPhone phone) is a parameter. It's saying whoever calls this method must give me a CellPhone object to work with.
+        System.out.println(phone.CellPhoneInfo());
+    }
+
 }
+
